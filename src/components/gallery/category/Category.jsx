ef6a64bc8d078c221231c7aa99664c01b3cfb432
unknown <madrigalamar@gmail.com>
2023-06-19 09:45:09 +0300

    before animation and prety adaptive

--- src/components/gallery/category/Category.jsx
+++ src/components/gallery/category/Category.jsx
@@ -5,7 +5,7 @@ const Category = ({ img1, img2, description }) => {
     <div className="Category__container">
       <div className="Category__img-container">
         <img className="Category__img1" src={img1} alt="img" />
-        <img src={img2} alt="img" />
+        <img className="Category__img2"src={img2} alt="img" />
       </div>
       <div className="Category__description">{description}</div>
     </div>
