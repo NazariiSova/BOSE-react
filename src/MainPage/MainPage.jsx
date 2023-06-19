97322d28322ed3f7a245fe0231c7cd14b0e0ff76
Nazarii Sova <madrigalamar@gmail.com>
2023-06-19 21:48:28 +0300

    before test

--- src/MainPage/MainPage.jsx
+++ src/MainPage/MainPage.jsx
@@ -3,35 +3,44 @@ import "./MainPage.scss";
 import Header from "../components/header/header.jsx";
 import Catalog from "../components/catalog/Catalog.jsx";
 import Gallery from "../components/gallery/Gallery.jsx";
-import footer from "../components/img/footer.png"
+import footer from "../components/img/footer.png";
 const MainPage = () => {
   return (
     <div className="main__container">
-      <Header  />
+      <Header />
       <Catalog />
       <Gallery />
       <div className="buy__container" id="buy">
-        <div className="buy__title">Why buy direct<br/> from Bose</div>
-        <div className="buy__description-container">
-          <ul className="buy__description-list">
-            <li className="buy__description-item">
-              Free 2-day shipping and returns
-            </li>
-            <li className="buy__description-item">90-day risk-free trial</li>
-            <li className="buy__description-item">
-              World class customer service
-            </li>
-            <li className="buy__description-item">My Bose account management</li>
-          </ul>
-          <div className="buy__description-text">
-            A great product is more than what’s in the box. It’s a promise of<br/>
-            premium performance, world-class support, and everything you<br/> expect
-            from a trusted brand. It’s just one of many reasons why<br/> you’ll shop
-            with confidence on Bose.com.
+        <div className="buy__title">
+          Why buy direct
+          <br /> from Bose
+        </div>
+        <div className="buy__container-block">
+          <div className="buy__description-container">
+            <ul className="buy__description-list">
+              <li className="buy__description-item">
+                Free 2-day shipping and returns
+              </li>
+              <li className="buy__description-item">90-day risk-free trial</li>
+              <li className="buy__description-item">
+                World class customer service
+              </li>
+              <li className="buy__description-item">
+                My Bose account management
+              </li>
+            </ul>
+            <div className="buy__description-text">
+              A great product is more than what’s in the box. It’s a promise of
+              <br />
+              premium performance, world-class support, and everything you
+              <br /> expect from a trusted brand. It’s just one of many reasons
+              why
+              <br /> you’ll shop with confidence on Bose.com.
+            </div>
           </div>
         </div>
       </div>
-      <img className="footer__photo" src={footer}/>
+      <img className="footer__photo" src={footer} alt="footer" />
     </div>
   );
 };
